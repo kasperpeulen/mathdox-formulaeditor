15717608928fb298d0b44e9a81954eb7ab1fceab
jwk <jwk@14ab19c6-35fa-0310-87bb-89b08bfd370d>
2009-01-20 10:27:05 +0000

    added permutation sgn and left_compose
    git-svn-id: https://mathdox.org/svn/repos/public/formulaeditor@11900 14ab19c6-35fa-0310-87bb-89b08bfd370d

--- source/org/mathdox/formulaeditor/FormulaEditor.js
+++ source/org/mathdox/formulaeditor/FormulaEditor.js
@@ -54,6 +54,7 @@ $require("org/mathdox/formulaeditor/modules/logic1/implies.js");
 $require("org/mathdox/formulaeditor/modules/logic1/not.js");
 $require("org/mathdox/formulaeditor/modules/logic1/or.js");
 
+$require("org/mathdox/formulaeditor/modules/permutation1/left_compose.js");
 $require("org/mathdox/formulaeditor/modules/permutation1/permutation.js");
 
 $require("org/mathdox/formulaeditor/modules/relation1/approx.js");
